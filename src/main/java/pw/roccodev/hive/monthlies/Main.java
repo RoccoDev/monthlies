5b18cdc21da174cce1f4dbdfc31de557610e0810
RoccoDev <roccodev.business@gmail.com>
2018-12-16 18:32:26 +0100

    Removed resets

--- src/main/java/pw/roccodev/hive/monthlies/Main.java
+++ src/main/java/pw/roccodev/hive/monthlies/Main.java
@@ -3,7 +3,6 @@ package pw.roccodev.hive.monthlies;
 import pw.roccodev.beezig.hiveapi.wrapper.GlobalConfiguration;
 import pw.roccodev.hive.monthlies.firebase.Database;
 import pw.roccodev.hive.monthlies.scheduler.Scheduler;
-import pw.roccodev.hive.monthlies.utils.ResetMode;
 
 public class Main {
 
@@ -12,13 +11,6 @@ public class Main {
         Scheduler.init();
         System.out.println("Running");
         GlobalConfiguration.setUserAgent("Monthlies Worker/1.0");
-
-        ResetMode.run(SupportedGame.CAI);
-        ResetMode.run(SupportedGame.GNT);
-        ResetMode.run(SupportedGame.GNTM);
-        ResetMode.run(SupportedGame.BP);
-        ResetMode.run(SupportedGame.SKY);
-        ResetMode.run(SupportedGame.HIDE);
     }
 
 }
